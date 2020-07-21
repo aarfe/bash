1620e02e4f65345ff9fd380576e225f4c4e7b91c
aarfe <aarfe2@gmail.com>
2020-07-21 12:25:37 +0200

    Start and stop functionality

--- tmux_airflow.sh
+++ tmux_airflow.sh
@@ -10,34 +10,64 @@ SESSIONE_NAME='airflow'
 WINDOW_NAME='window_1'
 PANE0_NAME='webserver'
 PANE1_NAME='scheduler'
-
-# Activate Venv with python dependencies
-source ~/Envs/venv_airflow/bin/activate
-
-# Create new session with name
-tmux new-session -d -s ${SESSIONE_NAME} -n ${WINDOW_NAME}
-
-# Split the window in 2 panes: 0 and 1
-tmux split-window -h -t "${WINDOW_NAME}"
-# Set top status bar on for the panes
-tmux set -g pane-border-status top
-
 # Manually set panes id to 0 and 1. It could be done automatically
 PANE_ID_WEBSERVER=0
 PANE_ID_SCHEDULER=1
 
-# Assign names to panes, to see them on the status bar
-tmux select-pane -T ${PANE0_NAME} -t ${PANE_ID_WEBSERVER}
-tmux select-pane -T ${PANE1_NAME} -t ${PANE_ID_SCHEDULER}
+usage()
+{
+    echo "usage: airflow_tmux [start | stop]"
+}
 
-# Send commands to the Panes
-tmux send-keys -t ${PANE_ID_WEBSERVER} 'source ~/Envs/venv_airflow/bin/activate' Enter
-tmux send-keys -t ${PANE_ID_WEBSERVER} 'airflow webserver' Enter #OK
+tmuxstart() {
+    # If the session already exists, attach and retun 0
+    tmux ls | grep $SESSIONE_NAME && { tmux a -t $SESSIONE_NAME; return 0; }
+    
+    # Activate Venv with python dependencies
+    source ~/Envs/venv_airflow/bin/activate
 
-tmux send-keys -t ${PANE_ID_SCHEDULER} 'source ~/Envs/venv_airflow/bin/activate' Enter
-tmux send-keys -t ${PANE_ID_SCHEDULER} 'airflow scheduler' Enter
+    # Create new session with name
+    tmux new-session -d -s ${SESSIONE_NAME} -n ${WINDOW_NAME}
 
-tmux -2 attach-session -d -t ${SESSIONE_NAME}
+    # Split the window in 2 panes: 0 and 1
+    tmux split-window -h -t "${WINDOW_NAME}"
+    # Set top status bar on for the panes
+    tmux set -g pane-border-status top
 
-# Scrollbar
-# ctrl-B + [    --> pgUp pgDown
+    # Assign names to panes, to see them on the status bar
+    tmux select-pane -T ${PANE0_NAME} -t ${PANE_ID_WEBSERVER}
+    tmux select-pane -T ${PANE1_NAME} -t ${PANE_ID_SCHEDULER}
+
+    # Send commands to the Panes
+    tmux send-keys -t ${PANE_ID_WEBSERVER} 'source ~/Envs/venv_airflow/bin/activate' Enter
+    tmux send-keys -t ${PANE_ID_WEBSERVER} 'airflow webserver' Enter #OK
+
+    tmux send-keys -t ${PANE_ID_SCHEDULER} 'source ~/Envs/venv_airflow/bin/activate' Enter
+    tmux send-keys -t ${PANE_ID_SCHEDULER} 'airflow scheduler' Enter
+
+    tmux -2 attach-session -d -t ${SESSIONE_NAME}
+    
+}
+
+tmuxstop() {
+    # Stop Airflow processes
+    tmux send-keys -t ${PANE_ID_WEBSERVER} 'C-c' Enter
+    tmux send-keys -t ${PANE_ID_SCHEDULER} 'C-c' Enter
+    # Kill tmux session
+    tmux kill-session -t ${SESSIONE_NAME}
+}
+
+# MAIN
+if [ $# == 1 ]; then
+    ACTION=$1
+    case $ACTION in 
+        start)
+            tmuxstart
+            ;;
+        stop)
+            tmuxstop
+            ;;
+    esac
+else
+    usage
+fi
